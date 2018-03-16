6ae868c3d43f7f55a0566f737ca6e4a3fa44665d
unknown <wangjialong@corp.netease.com>
2018-03-16 14:45:57 +0800

    test history and hash

---
+++ src/components/Pre.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="top">{{msg}}</div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            msg: '我是预览内容预览内容'
+        }
+    },
+    created() {
+        document.querySelector('title').innerText = '我是预览内容'
+    }
+}
+</script>
